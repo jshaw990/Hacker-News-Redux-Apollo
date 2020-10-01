28a29b48d36d6c69ecbc5f5a2399c776fcc9704a
Jayden Shaw <jayden.shaw@gmail.com>
2020-10-01 11:33:44 -0600

    Initial implementation of link voting. Added utility to show how long since link was added to app. User who posted link is also included.

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+
+import { AUTH_TOKEN } from '../constants'
+import { timeDifferenceForDate } from '../utils'
+
+const VOTE_MUTATION = gql`
+    mutation VoteMutation($linkId: ID!) {
+        id
+        link {
+            id
+            votes {
+                id
+                user {
+                    id
+                }
+            }
+        }
+        user {
+            id
+        }
+    }
+`
 
 class Link extends Component {
     render() {
-        return (
-            <div>
-                <div>
-                    {this.props.link.description} ({this.props.link.url})
+        const authToken = localStorage.getItem(AUTH_TOKEN)
+        return(
+            <div className="flex mt2 items-start">
+                <div className="flex items-center">
+                    <Mutation mutation={VOTE_MUTATION} 
+                    variables={{ linkId: this.props.link.id }} 
+                    update={(store, { data: { vote } }) => 
+                        this.props.updateStoreAfterVote(store, vote, this.props.link.id)
+                    }>
+                        {voteMutation => (
+                            <div className="ml1 gray fl1" onClick={voteMutation}>
+                                ★
+                            </div>
+                        )}
+                    </Mutation>
+                </div>
+                <div className="ml1">
+                    <div>
+                        {this.props.link.description} ({this.props.link.url})
+                    </div>
+                    <div className="f6 lh-copy gray">
+                        {this.props.link.votes.length} votes | by{' '}
+                        {this.props.link.postedBy
+                            ? this.props.link.postedBy.name 
+                            : 'Unknown'}{' '}
+                        {timeDifferenceForDate(this.props.link.createdAt)}
+                    </div>
                 </div>
             </div>
         )
